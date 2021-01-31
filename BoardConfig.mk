372d2205e8880053d5f7b740161e8d3c4acaf5dd
derfelot <derfelot.git@gmail.com>
2021-01-31 03:02:59 +0100

    poplar_dsds: Make use of TARGET_VENDOR_PROP

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ### VNDK
 BOARD_VNDK_VERSION := current
 
-### SYSTEM PROPS
+### PROPS
 # Add device-specific ones
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
